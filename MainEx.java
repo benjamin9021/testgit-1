08cbf33c41b9772d997c75e3ba45bad6d7694f26
ca <ca1234@naver.com>
2020-01-10 16:16:22 +0900

    MMS

---
+++ MainEx.java
@@ -0,0 +1,6 @@
+﻿
+public class MainEx {
+	public static void main(String[] args) {
+		new MemberDAO().update(new MemberDTO("m005", "lee", "d002", "19800101"));
+	}
+}
---
+++ MemberDAO.java
@@ -0,0 +1,46 @@
+﻿import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class MemberDAO {
+	private static final String DRIVE = "oracle.jdbc.driver.OracleDriver";
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String USER = "lwy";
+	private static final String PASS = "1234";
+	
+	public MemberDAO() {
+		try {
+			Class.forName(DRIVE);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public void update(MemberDTO dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "update member set name = ?, did = ?, birth = ? where id = ?";
+		
+		try {
+			conn = DriverManager.getConnection(URL, USER, PASS);
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getName());
+			pstmt.setString(2, dto.getDid());
+			pstmt.setString(3, dto.getBirth());
+			pstmt.setString(4, dto.getId());
+			pstmt.executeUpdate();
+			System.out.println("회원정보가 수정되었습니다.");
+			
+		} catch (Exception e) {
+			System.out.println("회원정보 수정에 실패하였습니다.");
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+	
+}
